c68e4e84da4da58acef3a64fcc2895a0808a5402
jaeseon0315 <jaeseon0315@gmail.com>
2022-02-07 15:50:14 +0900

    웹소켓 근데 이상한

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,47 @@
-helloddfssdffdgdfggfhfgh
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("#message")
+const nicknameForm = document.querySelector("#nickname")
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg)
+}
+
+
+socket.addEventListener("open", () => {
+    console.log("Connected to server")
+})
+
+socket.addEventListener("message", (message) => {
+    const li = document.createElement("li")
+    li.innerText = message.data
+    messageList.append(li)
+})
+
+socket.addEventListener("close", () => {
+    console.log("unConneted to server")
+})
+
+
+function handleSubmit(event){
+    event.preventDefault()
+    const input = messageForm.querySelector("input")
+    socket.send(makeMessage("new_message", input.value))
+    const li = document.createElement("li")
+    li.innerText = `나 : ${input.value}`
+    messageList.append(li)
+    input.value = ""
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault()
+    const input = nicknameForm.querySelector("input")
+    socket.send(makeMessage("nickname", input.value))
+    input.value = ""
+}
+
+messageForm.addEventListener("submit",handleSubmit)
+
+
+nicknameForm.addEventListener("submit",handleNicknameSubmit)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+import http from "http"
+import WebSocket from "ws"
 import express from "express"
-import res from "express/lib/response"
 
 const app = express()
 
@@ -10,4 +11,31 @@ app.get("/", (req, res) => res.render("home"))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 
-app.listen(3000, handleListen)
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+function onSocketClosing(){
+    console.log("연결 해제됨.")
+}
+
+const sockets = []
+
+wss.on("connection",(socket) => {
+    sockets.push(socket)
+    socket["nickname"] = "익명"
+    console.log("연결됨 !")
+    socket.on("close" , onSocketClosing)
+    socket.on("message", (message) => {
+        const msg = JSON.parse(message)
+        switch(msg.type){
+            case "new_message" : 
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${msg.payload}`))
+            case "nickname" : 
+                socket["nickname"] = msg.payload
+        }
+    })
+    socket.send("서버에 접속되었습니다.")
+})
+
+
+server.listen(3000, handleListen)
